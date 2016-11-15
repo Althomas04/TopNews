501be27d39d9f482e76850ac511b07f6eeb9a60d
Albert Thomas <Al.thomas04@gmail.com>
2016-11-15 01:21:14 -0500

    Fixed actionbar issue for webview. Replaced the custom created "action bar" with the original built-in action bar. Also, added share menu option onto webview activity, which will allow users to share articles using implicit intent.

--- app/src/main/java/app/com/example/althomas04/topnews/WebViewActivity.java
+++ app/src/main/java/app/com/example/althomas04/topnews/WebViewActivity.java
@@ -5,30 +5,30 @@ package app.com.example.althomas04.topnews;
  */
 
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageButton;
 
-public class WebViewActivity extends Activity {
+public class WebViewActivity extends AppCompatActivity {
 
     private WebView webView;
+    private ShareActionProvider mShareActionProvider;
 
     private String articleUrl = MainActivity.newsArticleUrl;
-    private ImageButton backButton;
 
     //Creates a WebView to display the full article within the app
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.web_view);
-
-        // Calls listener method for custom up button
-        addListenerOnButton();
 
         webView = (WebView) findViewById(R.id.web_view_layout);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -37,22 +37,31 @@ public class WebViewActivity extends Activity {
         webView.setWebViewClient(new MyCustomWebViewClient());
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         webView.loadUrl(articleUrl);
-
     }
 
-    // Custom UP button that navigates back to the main activity.
-    // Displays the same state the main activity was in before the webview was initialized.
-    public void addListenerOnButton() {
-        backButton = (ImageButton) findViewById(R.id.arrow_back_button);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                Intent intent = NavUtils.getParentActivityIntent(WebViewActivity.this);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                NavUtils.navigateUpTo(WebViewActivity.this, intent);
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.webview_menu, menu);
+        // Retrieve the share menu item
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        // Get the provider and hold onto it to set/change the share intent.
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareArticleIntent());
+        }
+
+        return true;
     }
+
+    private Intent createShareArticleIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, articleUrl);
+        return shareIntent;
+    }
+
 
     // IF the user further navigates within the webview, pressing the device back button will
     // allow users to navigate back within the webview itself rather than bringing them back to the main activity.
